b1698c87a889ba7497177b9da7c6cf2d93c6708a
Sergio Alfaro <sergioalfaro2807@gmail.com>
2020-10-07 10:07:05 -0600

    Portalio ended

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const options = {
+    bottom: '8rem', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '1rem', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff', // default: '#fff'
+    buttonColorDark: '#100f2c', // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+
+if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+    var typed = new Typed('.text-slider', {
+        strings: typed_strings.split(','),
+        typeSpeed: 80,
+        loop: true,
+        backDelay: 1100,
+        backSpeed: 30
+    });
+}
